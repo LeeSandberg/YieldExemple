5f18a5b443dce8c786fe28f567883cb5de7fc84a
LeeSandberg <Lee@Lees-MacBook-Pro.local>
2014-10-09 13:52:51 +0200

    Simple Yield demo example Unity Pro 4_5_4 projekt
    One sphere waits until a cube has moves and then the sphere moves. Like
    a character waits until a door is really opened over some frames.

---
+++ Assets/MoveYieldScript.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveYieldScript : MonoBehaviour {
+
+	// När man skriver public, ser man i editorn här (type ahead) vilka andra skript som finns publika
+	// skriv bara publik på en egen rad, så ser du listan.
+	public MoveYieldScriptCube cubescript; // den här används inte all i detta projekt.
+
+	// Use this for initialization
+	void Start () 
+	{
+		StartCoroutine("moveShpere");
+	}
+
+	
+	// Yield coroutine
+	private IEnumerator moveShpere()
+	{
+		float speed = 10.0f;
+
+/*		if (cube != null) 
+		{
+			cubescript.StartCoroutine("publicMoveCube");
+		}*/
+
+		// Vi  har lagt moveCube skriptet här i stället för i MoveYieldSctiptCube.cs
+		yield return StartCoroutine("moveCube");
+
+		for (int i=0; i<=25; i++)
+		{
+			float distance = speed * (float )Time.deltaTime * 2.0f; // Input.GetAxis("Horizontal");
+			
+			transform.Translate(Vector3.right * distance);
+			
+			yield return null; 
+		}
+	}
+
+	// Vi  har lagt moveCube skriptet här i stället för i MoveYieldSctiptCube.cs
+	// Att köra moveCube i MoveYieldSctiptCube från detta skript var lite svårare.
+	// Går igenom dom stegen senare.
+	// Yield coroutine
+	private IEnumerator moveCube()
+	{
+		float speed = 10.0f;
+		
+		GameObject box2; 
+		
+		box2 = GameObject.Find("Cube");
+		
+		for (int i=0; i<=50; i++)
+		{
+			float distance = speed * (float) Time.deltaTime * 0.2f;
+			
+			box2.transform.Translate(Vector3.up * distance);
+	
+			
+			yield return null; 
+		}
+	}
+
+
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+
+}
---
+++ Assets/MoveYieldScriptCube.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveYieldScriptCube : MonoBehaviour {
+
+	// Use this for initialization, överflödig funktion - ta bort.
+	void Start () 
+	{
+	
+	}
+
+	// publicMoveCube() funktionen fick vi lägga till mellan gameobjektens skript, då: 
+	// coroutiner är privata och inte publika. 
+	// så denna extar funktion är publik åt coroutinen.
+	// Så den syns utefrån av andra gameobjects skripts.
+	// andra gameobjekts skript.
+	// Dock krävs det att dom andra gameobjektens script har 
+	// "public MoveYieldScriptCube valfrittnamn;" i det andra gameobjektes skript,
+	// annars syns det inte.
+
+	public void publicMoveCube()  
+	{
+		StartCoroutine("moveCube");
+	}
+
+	// moveCube här används inte just nu. Utan är flyttad till MoveYieldScript.cs
+	// Används den så rör sig kuben åt andra hållet... (la till ett minus tecken här)
+	// Yield coroutine
+	private IEnumerator moveCube()
+	{
+		float speed = 10.0f;
+		
+		for (int i=0; i<=50; i++)
+		{
+			float distance = speed * (float) Time.deltaTime * -0.2f; // -2.0f; var 2.0f;
+
+			transform.Translate(Vector3.up * distance);
+			
+			yield return null; 
+		}
+	}
+	
+	// Update is called once per frame, överflödig funktion - ta bort.
+	void Update () {
+	
+	}
+}
